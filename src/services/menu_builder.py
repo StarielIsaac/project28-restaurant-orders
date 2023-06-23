6d8d3a42aa765143708f29606dd136b2ed56ded7
Stariel Isaac <starielisaac@gmail.com>
2023-06-23 15:21:57 -0300

    feat: implemented method get_main_menu in class MenuBuilder

--- src/services/menu_builder.py
+++ src/services/menu_builder.py
@@ -26,4 +26,15 @@ class MenuBuilder:
 
     # Req 4
     def get_main_menu(self, restriction=None) -> List[Dict]:
-        pass
+        main_menu = [
+            {
+                "dish_name": dish.name,
+                "price": dish.price,
+                "restrictions": list(dish.get_restrictions()),
+                "ingredients": list(dish.get_ingredients()),
+            }
+            for dish in self.menu_data.dishes
+            if restriction not in dish.get_restrictions()
+        ]
+
+        return main_menu
